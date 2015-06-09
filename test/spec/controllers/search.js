a9fb8b5388c7b60907007327db337546ad6583e9
Yuan <winsonyuan1@gmail.com>
2015-06-09 13:22:58 -0500

    updated test

--- test/spec/controllers/search.js
+++ test/spec/controllers/search.js
@@ -19,4 +19,16 @@ describe('Controller: SearchController', function () {
   it('should default search type to album', function () {
     expect(scope.types).toBe('album');
   });
+
+  // describe( '$scope.runSearch' ,function() {
+
+  //   it( 'should update the browser location URL to the search query and types', function() {
+  //     scope.searchQuery = 'biggie';
+  //     scope.types = 'artist';
+  //     scope.runSearch();
+  //     expect( location.path() ).toBe( '/search/artist/biggie' );
+
+  //   });
+
+  // });
 });
